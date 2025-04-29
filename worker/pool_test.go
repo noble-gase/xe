3e5dc2eafdbc5f313b3320f2a533c91829adb448
盛辉 <hui.sheng@sunmi.com>
2025-04-29 20:13:00 +0800

    更新pool test

--- worker/pool_test.go
+++ worker/pool_test.go
@@ -6,9 +6,11 @@ import (
 	"sync"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestSyncNormal(t *testing.T) {
+func TestNormal(t *testing.T) {
 	ctx := context.Background()
 	p := New(2)
 	defer p.Close()
@@ -35,7 +37,7 @@ func sleep1s(context.Context) {
 	time.Sleep(time.Second)
 }
 
-func TestSyncLimit(t *testing.T) {
+func TestLimit(t *testing.T) {
 	ctx := context.Background()
 	var wg sync.WaitGroup
 	// 没有并发数限制
@@ -70,7 +72,7 @@ func TestSyncLimit(t *testing.T) {
 	}
 }
 
-func TestSyncRecover(t *testing.T) {
+func TestRecover(t *testing.T) {
 	ch := make(chan struct{})
 	defer close(ch)
 	p := New(2, WithPanicHandler(func(ctx context.Context, err interface{}, stack []byte) {
@@ -86,78 +88,33 @@ func TestSyncRecover(t *testing.T) {
 	<-ch
 }
 
-func TestAsyncNormal(t *testing.T) {
-	ctx := context.Background()
-	p := New(2)
+func TestBlockTimeout(t *testing.T) {
+	p := New(1, WithCacheSize(1), WithBlockTimeout(time.Second))
 	defer p.Close()
-	m := make(map[int]int)
-	for i := range 4 {
-		m[i] = i
-	}
-	var wg sync.WaitGroup
-	wg.Add(1)
-	_ = p.Go(ctx, func(context.Context) {
-		m[1]++
-		wg.Done()
-	})
-	wg.Add(1)
-	_ = p.Go(ctx, func(context.Context) {
-		m[2]++
-		wg.Done()
-	})
-	wg.Wait()
-	t.Log(m)
-}
 
-func TestAsyncLimit(t *testing.T) {
-	ctx := context.Background()
-	var wg sync.WaitGroup
-	// 没有并发数限制
-	now := time.Now()
-	for i := 0; i < 4; i++ {
-		wg.Add(1)
-		go func() {
-			sleep1s(ctx)
-			wg.Done()
-		}()
-	}
-	wg.Wait()
-	sec := math.Round(time.Since(now).Seconds())
-	if sec != 1 {
-		t.FailNow()
-	}
-	// 限制并发数
-	p := New(2)
-	defer p.Close()
-	now = time.Now()
-	for range 4 {
-		wg.Add(1)
-		_ = p.Go(ctx, func(ctx context.Context) {
-			sleep1s(ctx)
-			wg.Done()
-		})
-	}
-	wg.Wait()
-	sec = math.Round(time.Since(now).Seconds())
-	if sec != 2 {
-		t.FailNow()
-	}
-}
-
-func TestAsyncRecover(t *testing.T) {
-	ch := make(chan struct{})
-	defer close(ch)
-	p := New(2, WithPanicHandler(func(ctx context.Context, err interface{}, stack []byte) {
-		t.Log("[error] panic recovered:", err)
-		t.Log("[stack]", string(stack))
-		ch <- struct{}{}
-	}))
-	defer p.Close()
-	_ = p.Go(context.Background(), func(ctx context.Context) {
-		sleep1s(ctx)
-		panic("oh my god!")
+	// 正常执行
+	err := p.Go(context.Background(), func(ctx context.Context) {
+		time.Sleep(2 * time.Second)
 	})
-	<-ch
+	assert.Nil(t, err)
+
+	// 等待队列
+	err = p.Go(context.Background(), func(ctx context.Context) {
+		time.Sleep(2 * time.Second)
+	})
+	assert.Nil(t, err)
+
+	// 入缓存链表
+	err = p.Go(context.Background(), func(ctx context.Context) {
+		time.Sleep(2 * time.Second)
+	})
+	assert.Nil(t, err)
+
+	// 阻塞超时
+	err = p.Go(context.Background(), func(ctx context.Context) {
+		time.Sleep(2 * time.Second)
+	})
+	assert.ErrorIs(t, ErrBlockTimeout, err)
 }
 
 func TestPoolClose(t *testing.T) {
