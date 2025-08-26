cea2a1d3fbf7b05e973f40c6d4603c3b1e0ff606
盛辉 <hui.sheng@sunmi.com>
2025-08-26 14:52:51 +0800

    实现分层时间轮

--- timewheel/option.go
+++ timewheel/option.go
@@ -7,7 +7,7 @@ import (
 // Option 时间轮选项
 type Option func(tw *timewheel)
 
-// WithCancelFn 指定任务 context「取消/超时」的处理方法
+// WithCancelFn 指定任务 context「取消｜超时」的处理方法
 func WithCancelFn(fn CancelFn) Option {
 	return func(tw *timewheel) {
 		tw.cancelFn = fn
